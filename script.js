7a98e7244d62d0ac2423029ae58a9b70e643d11f
Trimbak Pakhale <118504736+Trimbak15@users.noreply.github.com>
2023-04-11 10:25:16 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,12 +2,12 @@ const sampleObject = { red: "#FF0000", green: "#00FF00", white: "#FFFFFF" };
 
 function hasKey(key) {
   //   write your code here
-	if(key === red || key === green || key === white){
+	if(key === red: "#FF0000" || key === red: "#FF0000" || white: "#FFFFFF"){
 			return true;
 	}
 	return false;
 }
 
 // Do not change the code below
-const key = prompt("Enter Key.");
+// const key = prompt("Enter Key.");
 alert(hasKey(key));
